f6e21a24fed4a21fea1665c19f517c2713d00468
jyu9551 <jyu9551@naver.com>
2024-05-28 09:34:07 +0900

    "vs code commit"

---
+++ source/C/ch03_loop/ex3.1_산술연산자.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// 산술연산자 + - * / %
+int main(){
+    int n1 = 33, n2 = 10;
+
+    printf("%d\n", n1 / n2);
+    printf("%.2lf\n", (double)n1 / n2);
+    printf("%d\n", n1 % n2);
+}
